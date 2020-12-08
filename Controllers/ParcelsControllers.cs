acdfcd623f10300b5b6d35e78f3ea06b45d5d95e
AndrewMMickel <andrew.m.mickel@gmail.com>
2020-12-08 14:00:17 -0800

    add Parcel Controller

--- Controllers/ParcelsControllers.cs
+++ Controllers/ParcelsControllers.cs
@@ -2,3 +2,28 @@ using Microsoft.AspNetCore.Mvc;
 using Parcels.Models;
 using System.Collections.Generic;
 
+namespace Parcels.Controllers
+{
+    public class ParcelsController : Controller
+    {
+        [HttpGet("/parcels")]
+        public ActionResult Index()
+        {
+            List<Parcel> allParcels = Parcel.GetAll();
+            return View(allParcels);
+        }
+
+        [HttpGet("/parcels/new")]
+        public ActionResult CreateForm()
+        {
+            return View();
+        }
+
+        [HttpPost("/parcels")]
+        public ActionResult Create(int width, int height, int depth)
+        {
+            Parcel myParcel = new Parcel(width, height, depth);
+            return RedirectToAction("Index");
+        }
+    }
+}
