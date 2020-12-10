aeb3579746d485556eb6ba230641b8ab635cb45e
AndrewMMickel <andrew.m.mickel@gmail.com>
2020-12-10 14:22:08 -0800

    add tests for CostToShip
    Co-authored-by: Brian Hensley <brian.hensley@fidgetech.org>

--- Models/Parcels.cs
+++ Models/Parcels.cs
@@ -2,35 +2,38 @@ using System.Collections.Generic;
 
 namespace Parcels.Models
 {
-  public class Parcel
-  {
-    private int _width;
-    private int _height;
-    private int _depth;
-    private static List<Parcel> _instances = new List<Parcel>();
-
-    public Parcel(int width, int height, int depth)
+    public class Parcel
     {
-      _width = width;
-      _height = height;
-      _depth = depth;
+        private int _width;
+        private int _height;
+        private int _depth;
+        private static List<Parcel> _instances = new List<Parcel>();
 
-      _instances.Add(this);
-    }
+        public Parcel(int width, int height, int depth)
+        {
+            _width = width;
+            _height = height;
+            _depth = depth;
 
-    public static List<Parcel> GetAll()
-    {
-      return _instances;
-    }
+            _instances.Add(this);
+        }
 
-    public int Volume()
-    {
-      return _width * _height * _depth; 
-    }
+        // Parcel.GetAll()
+        public static List<Parcel> GetAll()
+        {
+            return _instances;
+        }
 
-    public int CostToShip()
-    {
-      return Volume() / 3;
+        // newParcel.Volume()
+        public int Volume()
+        {
+            return _width * _height * _depth;
+        }
+
+        // newParcel.CostToShip()
+        public int CostToShip()
+        {
+            return Volume() / 3;
+        }
     }
-  }
 }
